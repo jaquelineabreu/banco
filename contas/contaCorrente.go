a0da0bdc154e04508023c93e5f0fc4d7937a3f36
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-01-23 22:12:57 -0300

    organizando arquivos

--- contas/contaCorrente.go
+++ contas/contaCorrente.go
@@ -1,1 +1,39 @@
 package contas
+
+
+type ContaCorrente struct{
+	titular string 
+	numeroAgencia int64
+	numeroConta int64
+	saldo float64
+}
+
+func (c *ContaCorrente) Sacar(valordoSaque float64) string{
+	podeSacar := valordoSaque > 0 && valordoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valordoSaque
+		return "Saque realizado com sucesso"
+	}else{
+		return "Saldo insuficiente"
+	}
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64){
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Deposito realizado com sucesso", c.saldo
+	}else{
+		return "Valor deposito é menor que zero", c.saldo
+	}
+
+}
+
+func (c *ContaCorrente) Transferir(valoDaTransferencia float64, contaDestino *ContaCorrente)bool{
+	if valoDaTransferencia < c.saldo && valoDaTransferencia > 0 {
+		c.saldo -= valoDaTransferencia
+		contaDestino.Depositar(valoDaTransferencia)
+		return true
+	}else{
+		return false
+	}
+}
--- main.go
+++ main.go
@@ -1,48 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type ContaCorrente struct{
-	titular string 
-	numeroAgencia int64
-	numeroConta int64
-	saldo float64
-}
-
-func (c *ContaCorrente) Sacar(valordoSaque float64) string{
-	podeSacar := valordoSaque > 0 && valordoSaque <= c.saldo
-	if podeSacar {
-		c.saldo -= valordoSaque
-		return "Saque realizado com sucesso"
-	}else{
-		return "Saldo insuficiente"
-	}
-}
-
-func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64){
-	if valorDoDeposito > 0 {
-		c.saldo += valorDoDeposito
-		return "Deposito realizado com sucesso", c.saldo
-	}else{
-		return "Valor deposito é menor que zero", c.saldo
-	}
-
-}
-
-func (c *ContaCorrente) Transferir(valoDaTransferencia float64, contaDestino *ContaCorrente)bool{
-	if valoDaTransferencia < c.saldo && valoDaTransferencia > 0 {
-		c.saldo -= valoDaTransferencia
-		contaDestino.Depositar(valoDaTransferencia)
-		return true
-	}else{
-		return false
-	}
-}
+	"github.com/jaquelineabreu/banco/contas"
+)
 
 func main(){	
 
-	contaDaIvete := ContaCorrente{titular:"Ivete", saldo: 500}
-	contaDoJorge := ContaCorrente{titular: "Jorge", saldo: 100}
+	contaDaIvete := contas.ContaCorrente{titular:"Ivete", saldo: 500}
+	contaDoJorge := contas.ContaCorrente{titular: "Jorge", saldo: 100}
 
 	status := contaDoJorge.Transferir(-200, &contaDaIvete)
 
