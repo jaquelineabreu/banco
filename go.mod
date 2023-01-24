123594483368089c176dcaa858265d43ba575367
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-01-24 15:44:32 -0300

    testando chamada com struct titular

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
-module go/banco
+module github.com/jaquelineabreu/banco
 
 go 1.19
-
-require github.com/jaquelineabreu/banco v0.0.0-20230124012951-2631231356e9
--- main.go
+++ main.go
@@ -4,18 +4,16 @@ import (
 	"fmt"
 
 	"github.com/jaquelineabreu/banco/contas"
+	"github.com/jaquelineabreu/banco/clientes"
 ) 
 func main(){	
 
-	contaDaIvete := contas.ContaCorrente{Titular: "Ivete", Saldo: 500}
-	contaDoJorge := contas.ContaCorrente{Titular: "Jorge", Saldo: 100}
+	contaDaMarisa := contas.ContaCorrente{Titular:clientes.Titular{
+		Nome: "Marisa",
+		CPF:"123.111.123.12",
+		Profissao: "Desenvolvedora"},
+		NumeroAgencia:123, NumeroConta:123456, Saldo:100}
 
-	status := contaDoJorge.Transferir(-200, &contaDaIvete)
-
-	fmt.Println(status)
-	fmt.Println(contaDaIvete)
-	fmt.Println(contaDoJorge)
-
-
+	fmt.Println(contaDaMarisa)
 	
 }
