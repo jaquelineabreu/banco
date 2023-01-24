1e16978b9470ea9ed7d5dc009353c724fa50040a
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-01-24 17:20:48 -0300

    criada a interface verificarConta

--- main.go
+++ main.go
@@ -6,12 +6,28 @@ import (
 	"github.com/jaquelineabreu/banco/contas"
 	
 ) 
+
+func PagarBoleto(conta verificarConta, valorDoBoleto float64){
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main(){	
 
 	contaDaMarilia := contas.ContaPoupanca{}
 	contaDaMarilia.Depositar(100)
-	contaDaMarilia.Sacar(45)
+	PagarBoleto(&contaDaMarilia, 60)
 
 	fmt.Println(contaDaMarilia.ObterSaldo())
+
+	contaDaLuiza := contas.ContaCorrente{}
+	contaDaLuiza.Depositar(500)
+	PagarBoleto(&contaDaLuiza, -400)
+
+	fmt.Println(contaDaLuiza.ObterSaldo())
+
 	
 }
