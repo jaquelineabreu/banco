2631231356e9bdd7c2045862f1978e7584f0984b
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-01-23 22:29:51 -0300

    fix - visibilidade

--- main.go
+++ main.go
@@ -4,12 +4,11 @@ import (
 	"fmt"
 
 	"github.com/jaquelineabreu/banco/contas"
-)
-
+) 
 func main(){	
 
-	contaDaIvete := contas.ContaCorrente{titular:"Ivete", saldo: 500}
-	contaDoJorge := contas.ContaCorrente{titular: "Jorge", saldo: 100}
+	contaDaIvete := contas.ContaCorrente{Titular: "Ivete", Saldo: 500}
+	contaDoJorge := contas.ContaCorrente{Titular: "Jorge", Saldo: 100}
 
 	status := contaDoJorge.Transferir(-200, &contaDaIvete)
 
